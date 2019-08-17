e2581439794d2562fbd11a5bc54ebf1034d6c132
Julian <Julian_cn@126.com>
2019-08-17 09:25:01 +0800

    install_zabbix V1.2

--- install_zabbix_server.sh
+++ install_zabbix_server.sh
@@ -28,7 +28,7 @@ function get_tar_file() {
     nginx_tar=`find / -name nginx*.tar.* | awk 'END{print $NF}'`
     if [ -z "${nginx_tar}" ]; then 
         wget https://github.com/Julian-ZLY/Yum_repo/raw/master/nginx-1.12.2.tar.gz -P /opt 
-	[ ! $? -eq 0 ] && echo -e "\033[31m\t下载失败;请检查网络是否通畅...\033[0m" ; exit 1 
+        [  $? -ne 0 ] && echo -e "\033[31m\t下载失败;请检查网络是否通畅...\033[0m`exit 1`"
     fi 
     
     zabbix_tar=`find / -name zabbix*.tar.* | awk 'END{print $NF}'`
